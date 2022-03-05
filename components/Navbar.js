2684a6c61aa5684dcdc20de875035e658cbe8c33
eternaleight <eternaleight0@gmail.com>
2022-03-06 08:42:00 +0900

    uber

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,32 @@
+import Image from "next/image"
+import avater from "../temp/名称未設定211q.png"
 
 const style = {
-  wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center`
+  wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center`,
+  leftMenu: `flex gap-3`,
+  logo: `text-3xl text-white flex cursor-pointer mr-16`,
+  menuItem: `text-lg text-white font-medium flex items-center mx-4 cursor-pointer`,
+  rightMenu: `flex gap-3 items-center`,
+  userImageContainer: `h-10 w-10`
 }
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className={style.wrapper}>
+      <div className={style.leftMenu}>
+        <div className={style.logo}>Uber</div>
+        <div className={style.menuItem}>Ride</div>
+        <div className={style.menuItem}>Drive</div>
+        <div className={style.menuItem}>More</div>
+      </div>
+      <div className={style.rightMenu}>
+        <div className={style.menuItem}>Help</div>
+        <div className={style.menuItem}>Lance</div>
+        <div className={style.userImageContainer}>
+          <Image src={avater} width={40} height={40} />
+        </div>
+      </div>
+    </div>
   )
 }
 
